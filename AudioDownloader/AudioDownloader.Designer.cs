2defc5b9e288a2f532760f58f81a190e92c1162c
Veggie13 <cmderochie@gmail.com>
2021-11-10 19:09:51 -0700

    Initial commit.

---
+++ AudioDownloader/AudioDownloader.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace AudioDownloader
+{
+    partial class AudioDownloader
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this._txtUrl = new System.Windows.Forms.TextBox();
+            this._btnSaveAs = new System.Windows.Forms.Button();
+            this._txtFilePath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this._btnDownload = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "URL:";
+            // 
+            // _txtUrl
+            // 
+            this._txtUrl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this._txtUrl.Location = new System.Drawing.Point(78, 12);
+            this._txtUrl.Name = "_txtUrl";
+            this._txtUrl.Size = new System.Drawing.Size(710, 39);
+            this._txtUrl.TabIndex = 1;
+            // 
+            // _btnSaveAs
+            // 
+            this._btnSaveAs.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this._btnSaveAs.Location = new System.Drawing.Point(638, 57);
+            this._btnSaveAs.Name = "_btnSaveAs";
+            this._btnSaveAs.Size = new System.Drawing.Size(150, 46);
+            this._btnSaveAs.TabIndex = 2;
+            this._btnSaveAs.Text = "Save As...";
+            this._btnSaveAs.UseVisualStyleBackColor = true;
+            this._btnSaveAs.Click += new System.EventHandler(this._btnSaveAs_Click);
+            // 
+            // _txtFilePath
+            // 
+            this._txtFilePath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this._txtFilePath.Location = new System.Drawing.Point(78, 61);
+            this._txtFilePath.Name = "_txtFilePath";
+            this._txtFilePath.Size = new System.Drawing.Size(554, 39);
+            this._txtFilePath.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 32);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "File:";
+            // 
+            // _btnDownload
+            // 
+            this._btnDownload.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this._btnDownload.Location = new System.Drawing.Point(12, 127);
+            this._btnDownload.Name = "_btnDownload";
+            this._btnDownload.Size = new System.Drawing.Size(776, 46);
+            this._btnDownload.TabIndex = 5;
+            this._btnDownload.Text = "DOWNLOAD";
+            this._btnDownload.UseVisualStyleBackColor = true;
+            this._btnDownload.Click += new System.EventHandler(this._btnDownload_Click);
+            // 
+            // AudioDownloader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 185);
+            this.Controls.Add(this._btnDownload);
+            this.Controls.Add(this._txtFilePath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this._btnSaveAs);
+            this.Controls.Add(this._txtUrl);
+            this.Controls.Add(this.label1);
+            this.Name = "AudioDownloader";
+            this.Text = "Audio Downloader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox _txtUrl;
+        private System.Windows.Forms.Button _btnSaveAs;
+        private System.Windows.Forms.TextBox _txtFilePath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button _btnDownload;
+    }
+}
+
---
+++ AudioDownloader/AudioDownloader.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+using YoutubeExplode;
+using YoutubeExplode.Videos.Streams;
+using System.Runtime.InteropServices;
+
+namespace AudioDownloader
+{
+    public partial class AudioDownloader : Form
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        struct FLASHWINFO
+        {
+            public UInt32 cbSize;
+            public IntPtr hwnd;
+            public UInt32 dwFlags;
+            public UInt32 uCount;
+            public UInt32 dwTimeout;
+        }
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool FlashWindowEx(ref FLASHWINFO pwfi);
+
+        bool flash()
+        {
+            var fInfo = new FLASHWINFO()
+            {
+                cbSize = Convert.ToUInt32(Marshal.SizeOf<FLASHWINFO>()),
+                hwnd = Handle,
+                dwFlags = 3 | 12,
+                uCount = UInt32.MaxValue,
+                dwTimeout = 0
+            };
+            return FlashWindowEx(ref fInfo);
+        }
+
+        public AudioDownloader()
+        {
+            InitializeComponent();
+        }
+
+        private void _btnSaveAs_Click(object sender, EventArgs e)
+        {
+            var dlg = new SaveFileDialog()
+            {
+                DefaultExt = "mp3",
+                AddExtension = true,
+                Filter = "MP3 Files (*.mp3)|*.mp3",
+                Title = "Save As..."
+            };
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                _txtFilePath.Text = dlg.FileName;
+            }
+        }
+
+        private async void _btnDownload_Click(object sender, EventArgs e)
+        {
+            _txtUrl.Enabled = false;
+            _txtFilePath.Enabled = false;
+            _btnSaveAs.Enabled = false;
+            _btnDownload.Enabled = false;
+
+            var youtube = new YoutubeClient();
+
+            var video = await youtube.Videos.GetAsync(_txtUrl.Text);
+            var manifest = await youtube.Videos.Streams.GetManifestAsync(video.Id);
+
+            var streamInfo = manifest.GetAudioOnlyStreams().GetWithHighestBitrate();
+            await youtube.Videos.Streams.DownloadAsync(streamInfo, _txtFilePath.Text);
+
+            _txtUrl.Enabled = true;
+            _txtFilePath.Enabled = true;
+            _btnSaveAs.Enabled = true;
+            _btnDownload.Enabled = true;
+
+            flash();
+        }
+    }
+}
